9cf2c98701b0cda27e2c55d91fe502f7242461f2
Connor McLean <c.mclean165@gmail.com>
2025-12-27 09:46:56 -0700

    WEB|ACTIONS: subscription check with better caching

--- apps/web/src/actions/subscriptions.tsx
+++ apps/web/src/actions/subscriptions.tsx
@@ -1,10 +1,11 @@
 "use server";
 
 import { cacheLife } from 'next/cache'
+import { connection } from 'next/server'
 import polarService from "@/server/polar";
 
-export async function getCustomerState( userId: string) {
-  "use cache";
+async function getCustomerStateWithCache(userId: string) {
+  'use cache'
   cacheLife('minutes')
   try {
     const customerState = await polarService.getCustomerStateExternal(userId);
@@ -12,4 +13,14 @@ export async function getCustomerState( userId: string) {
   } catch {
     return null;
   }
+}
+
+export async function getCustomerState(userId: string) {
+  try {
+    // Skip during prerendering
+    await connection();
+  } catch {
+    return null;
+  }
+  return await getCustomerStateWithCache(userId);
 }
