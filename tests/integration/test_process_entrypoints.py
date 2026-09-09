642a7167e7cd1d48220bae19e04405a29488b60b
Connor McLean <c.mclean165@gmail.com>
2026-09-09 03:14:46 -0600

    fixup! Fund execution from prepaid credit and settle verified payments

--- tests/integration/test_process_entrypoints.py
+++ tests/integration/test_process_entrypoints.py
@@ -131,6 +131,7 @@ def test_scheduler_runtime_closes_owned_services_on_exception(
         email_outbox=app_services.email_outbox,
         plan_changes=app_services.plan_changes,
         billing_reconciliation=app_services.billing_reconciliation,
+        billing_payments=app_services.billing_payments,
         billing_enforcement=app_services.billing_enforcement,
         retention=app_services.retention,
         custom_domains=app_services.custom_domains,
