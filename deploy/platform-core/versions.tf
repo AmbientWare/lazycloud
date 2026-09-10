b3597ded4361625893f65c6476e7b96f5ee5ffae
Connor McLean <c.mclean165@gmail.com>
2026-09-09 22:49:54 -0600

    Fix cache routing and Terraform authentication during Spot rollout

--- deploy/platform-core/versions.tf
+++ deploy/platform-core/versions.tf
@@ -37,7 +37,11 @@ provider "aws" {
 provider "kubernetes" {
   host                   = aws_eks_cluster.control_plane.endpoint
   cluster_ca_certificate = base64decode(aws_eks_cluster.control_plane.certificate_authority[0].data)
-  token                  = data.aws_eks_cluster_auth.control_plane.token
+  exec {
+    api_version = "client.authentication.k8s.io/v1beta1"
+    command     = "aws"
+    args        = ["eks", "get-token", "--cluster-name", var.name, "--region", var.region]
+  }
 }
 
 provider "helm" {
@@ -53,6 +57,10 @@ provider "helm" {
   kubernetes {
     host                   = aws_eks_cluster.control_plane.endpoint
     cluster_ca_certificate = base64decode(aws_eks_cluster.control_plane.certificate_authority[0].data)
-    token                  = data.aws_eks_cluster_auth.control_plane.token
+    exec {
+      api_version = "client.authentication.k8s.io/v1beta1"
+      command     = "aws"
+      args        = ["eks", "get-token", "--cluster-name", var.name, "--region", var.region]
+    }
   }
 }
